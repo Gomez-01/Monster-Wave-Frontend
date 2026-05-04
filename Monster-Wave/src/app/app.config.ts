fcdde980c714290802fd71ce8eee9329819bb43c
Gabriel G. Santos <Gabrielgs2uwu@gmail.com>
2026-05-04 06:51:38 +0000

    feat: switch PrimeNG theme from Aura to unstyled

--- Monster-Wave/src/app/app.config.ts
+++ Monster-Wave/src/app/app.config.ts
@@ -10,9 +10,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     providePrimeNG({
-        theme: {
-            preset: Aura
-        }
+      unstyled: true
     })
   ]
 };
